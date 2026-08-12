bb7bd50e1ce7916da1dccfa41f98bf64241a9435
ang3lak0s <angelakos20050511@gmail.com>
2026-08-12 15:41:06 +0200

    Initial Spring Boot Project

---
+++ src/main/java/hu/angel/parkingreservation/ParkingReservationApplication.java
@@ -0,0 +1,13 @@
+package hu.angel.parkingreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingReservationApplication.class, args);
+    }
+
+}
